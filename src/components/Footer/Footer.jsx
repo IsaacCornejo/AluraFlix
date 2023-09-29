b2ad84da8dcaf908c608238f219b260aba4c1edd
Isaac Cornejo <espinosacornejo.pr@gmail.com>
2023-09-28 20:25:13 -0600

    agregando enlace de portafolio al footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,10 +8,21 @@ const FooterContainer = styled.footer`
   width: 100%;
   background-color: black;
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   border-top: 1px solid red;
   padding: 1rem;
   box-sizing: border-box;
+  gap: 1rem;
+`;
+
+const Enlace = styled(Link)`
+  color: white;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 const Footer = () => {
@@ -22,6 +33,12 @@ const Footer = () => {
           <Link to={"/"}>
             <Logo src={LogoRestauraFlix} alt="Logo" />
           </Link>
+          <Enlace
+            to={"https://isaaccornejo.github.io/portfolio-alura-one/"}
+            target="_blank"
+          >
+            &copy; Desarrollado por Isaac C.
+          </Enlace>
         </FooterContainer>
       }
     </>
